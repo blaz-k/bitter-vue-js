56ed329c4db26c8fbdb1dd612dd68b5dc2b54fc4
blaz <blazyy@gmail.com>
2021-09-13 12:52:35 +0200

    button made, component added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <main>
+      <AllBitts />
+      
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/*
+import axios from "axios";*/
+import AllBitts from "./components/AllBitts";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    AllBitts,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/AllBitts.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Show all bitts</h2>
+    <button @click="getAllBitts">Gett All Bitts</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "AllBitts",
+  methods: {
+    async getAllBitts() {
+      let response = await axios.get("http://localhost:5000/get-all-bitts");
+
+      console.log(response.data);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
