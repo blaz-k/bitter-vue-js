9add93cf319110a58687e064103488ba6dc3f3cf
blaz <blazyy@gmail.com>
2021-09-15 10:19:33 +0200

    you can post new "bitt", wich shows when you press button >show all bitts<

--- src/components/AllBitts.vue
+++ src/components/AllBitts.vue
@@ -3,14 +3,14 @@
     <h2>Bitts forum</h2>
     <button class="showBittsButton" @click="getAllBitts">Show All Bitts</button>
     <div class="addNewBitt">
-     <input id="usernameInput" type="text" placeholder="Username" />
-     <input
-       id="textInput"
-       class="input-text"
-       type="text"
-       placeholder="Enter your text here"
-     />
-     <button @click="newBitt" class="newBittButton">Submit new bit</button>
+      <input id="usernameInput" type="text" placeholder="Username" />
+      <input
+        id="textInput"
+        class="input-text"
+        type="text"
+        placeholder="Enter your text here"
+      />
+      <button @click="newBitt" class="newBittButton">Submit new bit</button>
     </div>
 
     <div v-bind:key="bitt.id" v-for="bitt in bitts">
@@ -48,11 +48,17 @@ export default {
       console.log(response.data);
       this.bitts = response.data;
     },
-    newBitt() {
+    async newBitt() {
       let usernameInput = document.getElementById("usernameInput").value;
       let textInput = document.getElementById("textInput").value;
       console.log(textInput);
       console.log(usernameInput);
+      let res = await axios.post("http://localhost:5000/create-bitt", {
+        text: textInput,
+        username: usernameInput,
+      });
+      let data = res.data;
+      console.log(data);
     },
   },
 };
@@ -60,7 +66,7 @@ export default {
 
 <style scoped>
 .addNewBitt {
- margin-top: 50px;
+  margin-top: 50px;
 }
 .information {
   text-align: left;
@@ -92,7 +98,6 @@ export default {
   text-shadow: 0px 1px;
   font-size: 15px;
 }
-
 
 h2 {
   font-size: 40px;
