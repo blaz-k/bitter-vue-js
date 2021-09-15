a0a7f708c0d997f94595eaf9ad399435a3a8c801
blaz <blazyy@gmail.com>
2021-09-15 11:12:59 +0200

    implementing css in input and button

--- src/components/AllBitts.vue
+++ src/components/AllBitts.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>Bitts forum</h2>
+    <h2>Bitts</h2>
     <button class="showBittsButton" @click="getAllBitts">Show All Bitts</button>
     <div class="addNewBitt">
       <input id="usernameInput" type="text" placeholder="Username" />
@@ -78,6 +78,7 @@ export default {
   font-size: 22px;
   font-weight: 700;
   color: rgb(189, 8, 8);
+  margin-left: 10px;
 }
 
 .text {
@@ -88,11 +89,12 @@ export default {
   background-color: rgba(189, 165, 165, 0.733);
   border-radius: 13px;
   margin: 8px 0px;
+  margin-left: 10px;
 }
 .showBittsButton {
   display: flex;
   margin: auto;
-  margin-top: 50px;
+  margin-top: 20px;
   border-radius: 13px;
   padding: 5px 15px;
   text-shadow: 0px 1px;
@@ -100,8 +102,36 @@ export default {
 }
 
 h2 {
-  font-size: 40px;
-  height: 20px;
+  font-size: 100px;
+  font-weight: 900;
   text-align: center;
+}
+
+#usernameInput {
+  padding: 10px;
+  font-size: 20px;
+  border-radius: 0px 10px 0px 10px;
+  box-shadow: 0px 0px 26px;
+  border-color: rgb(255, 0, 0);
+  margin: 10px;
+}
+#textInput {
+  padding: 10px;
+  font-size: 20px;
+  border-radius: 0px 10px 0px 10px;
+  box-shadow: 0px 0px 26px;
+  border-color: rgba(189, 165, 165, 0.733);
+  margin: 10px;
+}
+.newBittButton {
+  margin: 10px;
+  border-radius: 10px;
+  padding: 11px;
+  text-shadow: 0px 1px;
+  font-size: 20px;
+  border-color: rgb(10, 10, 10);
+}
+.addNewBitt {
+  margin: 10px;
 }
 </style>
